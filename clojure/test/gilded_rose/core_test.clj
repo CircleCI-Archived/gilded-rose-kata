3ec868998712204d1778f6bf6c19cd026579dc14
Nate Smith <nwjsmith@gmail.com>
2016-06-23 06:46:30 -0400

    Use `core.test` instead of `speclj`
    `core.test` is provided by Clojure, and it is the most widely used
    testing framework for the language. Familiarity with `speclj` probably
    can't be assumed.

---
+++ clojure/test/gilded_rose/core_test.clj
@@ -0,0 +1,7 @@
+(ns gilded-rose.core-test
+  (:require [clojure.test :refer :all]
+            [gilded-rose.core :refer :all]))
+
+(deftest gilded-rose-test
+  (testing "TODO - write tests later"
+    (is (= 0 1))))
