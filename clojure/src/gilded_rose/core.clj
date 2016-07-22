187e05ae6010e46656acc9b4d4c236c8f299f996
AlexanderMann <mannimal09@gmail.com>
2016-07-22 00:35:57 -0400

    final commit

--- clojure/src/gilded_rose/core.clj
+++ clojure/src/gilded_rose/core.clj
@@ -1,59 +1,91 @@
 (ns gilded-rose.core)
 
+(def name-backstage-passes "Backstage passes to a TAFKAL80ETC concert")
+(def name-sulfuras "Sulfuras, Hand of Ragnaros")
+(def name-brie "Aged Brie")
+(def name-conjured "Conjured")
+
+(def quality-reduction 2)
+(def quality-min 0)
+(def quality-max 50)
+
 (defn dec-sell-in
   [{:keys [sell-in] :as item}]
   (assoc item
     :sell-in (dec sell-in)))
 
-(defn change-quality
-  [{:keys [sell-in quality] :as item} neg-quality-fn]
+(defn bound-quality
+  [{:keys [quality] :as item}]
   (assoc item
-    :quality (if (neg? sell-in)
-               (neg-quality-fn quality)
-               quality)))
+    :quality (-> quality
+                 (max quality-min)
+                 (min quality-max))))
 
 (defn update-quality
+  ([item]
+   (update-quality #(- % quality-reduction) item))
+  ([neg-quality-fn {:keys [sell-in quality] :as item}]
+   (bound-quality
+     (assoc item
+       :quality (if (neg? sell-in)
+                  (neg-quality-fn quality)
+                  quality)))))
+
+(defn backstage-passes-update-quality
+  [{:keys [sell-in quality] :as item}]
+  (bound-quality
+    (assoc item
+      :quality (cond (neg? sell-in)
+                     0
+
+                     (> 5 sell-in)
+                     (+ quality 3)
+
+                     (> 10 sell-in)
+                     (+ quality 2)
+
+                     :else
+                     (inc quality)))))
+
+(defn brie-update-quality
+  [{:keys [quality] :as item}]
+  (bound-quality
+    (assoc item
+      :quality (inc quality))))
+
+(defn sulfuras-update-quality
+  [item]
+  (assoc item
+    :quality 80))
+
+(defn update-items
   [items]
   (map
-    (fn [item] (cond
-                 (and (< (:sell-in item) 0)
-                      (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
-                 (merge item {:quality 0})
+    (fn [{:keys [name] :as item}]
+      (let [[update-quality-fn updated-sell-in-fn]
+            (condp = name
+              name-sulfuras
+              [sulfuras-update-quality
+               identity]
 
-                 (= "Conjured" (:name item))
-                 (-> item
-                     (change-quality #(- % 4))
-                     dec-sell-in)
+              name-backstage-passes
+              [backstage-passes-update-quality
+               dec-sell-in]
 
-                 (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5)
-                      (< (:sell-in item) 10))
-                 (merge item {:quality (inc (inc (:quality item)))})
+              name-brie
+              [brie-update-quality
+               dec-sell-in]
 
-                 (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0)
-                      (< (:sell-in item) 5))
-                 (merge item {:quality (inc (inc (inc (:quality item))))})
+              name-conjured
+              [(partial update-quality #(- % (* 2 quality-reduction)))
+               dec-sell-in]
 
-                 (#{"Aged Brie"
-                    "Backstage passes to a TAFKAL80ETC concert"}
-                   (:name item))
-                 (if (< (:quality item) 50)
-                   (merge item {:quality (inc (:quality item))})
-                   item)
-
-                 (neg? (:sell-in item))
-                 (if (#{"+5 Dexterity Vest"
-                        "Elixir of the Mongoose"}
-                       (:name item))
-                   (merge item {:quality (max (- (:quality item) 2)
-                                              0)})
-                   item)
-
-                 :else item))
-    (map (fn [item]
-           (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-             (merge item {:sell-in (dec (:sell-in item))})
-             item))
-         items)))
+              [update-quality
+               dec-sell-in])]
+        (-> item
+            updated-sell-in-fn
+            update-quality-fn)))
+    items))
 
 ;; BEGIN OFF LIMITS
 (defn item [item-name, sell-in, quality]
@@ -65,10 +97,10 @@
   (let [inventory
         [
          (item "+5 Dexterity Vest" 10 20)
-         (item "Aged Brie" 2 0)
+         (item name-brie 2 0)
          (item "Elixir of the Mongoose" 5 7)
-         (item "Sulfuras, Hand Of Ragnaros" 0 80)
-         (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
+         (item name-sulfuras 0 80)
+         (item name-backstage-passes 15 20)
          ]]
-    (update-quality inventory)
+    (update-items inventory)
     ))
