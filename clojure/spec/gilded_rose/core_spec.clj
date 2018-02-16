3a8591a8b0bdc518c7a8764ebb91308cf4a31e39
Justin Cowperthwaite <justinc474@gmail.com>
2018-02-16 15:43:11 -0800

    one last time

--- clojure/spec/gilded_rose/core_spec.clj
+++ clojure/spec/gilded_rose/core_spec.clj
@@ -3,4 +3,4 @@
           [gilded-rose.core :refer [update-quality item]]))
 
 (deftest gilded-rose-test
-  (is (= "fixme" (update-quality (item "foo" 0 0)))))
+  (is (= "fixme" (:name (first (update-quality [(item "foo" 0 0)]))))))
