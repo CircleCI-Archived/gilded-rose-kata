de6b4e517c3cbd2e870a7bb82bca57fb3acab408
Justin Cowperthwaite <justinc474@gmail.com>
2018-02-16 11:03:41 -0800

    better imports for a happier namespace

--- clojure/spec/gilded_rose/core_spec.clj
+++ clojure/spec/gilded_rose/core_spec.clj
@@ -1,9 +1,9 @@
 (ns gilded-rose.core-spec
 (:require [clojure.test :refer :all]
-          [gilded-rose.core :refer [update-quality]]))
+          [gilded-rose.core :refer [update-quality item]]))
 
 (deftest update-quality
   (testing "regular items"
     (testing "before sell-by date"
-      (is (= [(gr/item "foo" 4 6)]
-             (gr/update-quality [(gr/item "foo" 5 7)]))))))
+      (is (= [(item "foo" 4 6)]
+             (update-quality [(item "foo" 5 7)]))))))
