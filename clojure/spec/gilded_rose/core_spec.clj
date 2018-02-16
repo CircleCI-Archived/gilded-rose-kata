dc9d45a998422bcbccdc181194d7d64a86330aeb
Justin Cowperthwaite <justinc474@gmail.com>
2018-02-16 10:05:51 -0800

    add failing tests to the clojure kata
    every other language has one...

--- clojure/spec/gilded_rose/core_spec.clj
+++ clojure/spec/gilded_rose/core_spec.clj
@@ -1,6 +1,9 @@
 (ns gilded-rose.core-spec
-(:require [speclj.core :refer :all]
+(:require [clojure.test :refer :all]
           [gilded-rose.core :refer [update-quality]]))
 
-(describe "gilded rose"
-          (it "TODO - write tests later"))
+(deftest update-quality
+  (testing "regular items"
+    (testing "before sell-by date"
+      (is (= [(gr/item "foo" 4 6)]
+             (gr/update-quality [(gr/item "foo" 5 7)]))))))
