46411c7f1ce650206b6c31d91d74d35564d16925
Spencer Davis <spencerdavis2012@gmail.com>
2019-04-29 01:30:43 -0500

    red... GREEN! Tests passing. Refactor time.

--- test/test.js
+++ test/test.js
@@ -12,7 +12,7 @@ items.push(new gilded.Item('Backstage passes to a TAFKAL80ETC concert', 15, 20))
 items.push(new gilded.Item('Backstage passes to a TAFKAL80ETC concert', 15, 50)); // 6
 items.push(new gilded.Item('Backstage passes to a TAFKAL80ETC concert', 11, 0));  // 7
 items.push(new gilded.Item('Backstage passes to a TAFKAL80ETC concert', 6, 0));   // 8
-items.push(new gilded.Item('Backstage passes to a TAFKAL80ETC concert', 1, 50));  // 9
+items.push(new gilded.Item('Backstage passes to a TAFKAL80ETC concert', 0, 40));  // 9
 items.push(new gilded.Item('+5 Dexterity Vest', 0, 20));                          // 10
 
 
@@ -21,18 +21,18 @@ gilded.UpdateItems(items);
 // Test General item
 describe('+5 Dexterity Vest', () => {
   describe('(10,20) regular', () => {
-  it('should have sell_in 9 on day 1', () => {
+  it('should have sell_in 9', () => {
     assert.strictEqual(items[0].sell_in, 9, 'Failed');
   });
-  it('should have quality 19 on day 1', () => {
+  it('should have quality 19', () => {
     assert.strictEqual(items[0].quality, 19, 'Failed');
   });
   });
   describe('(0,20) double decay', () => {
-    it('should have sell_in -1 on day 1', () => {
+    it('should have sell_in -1', () => {
       assert.strictEqual(items[10].sell_in, -1, 'Failed');
     });
-    it('should have quality 18 on day 1', () => {
+    it('should have quality 18', () => {
       assert.strictEqual(items[10].quality, 18, 'Failed');
     });
     });
@@ -41,10 +41,10 @@ describe('+5 Dexterity Vest', () => {
 // Test Brie
 describe('Aged Brie', () => { 
   describe('(2,0) - quality increase', () => {
-    it('should have sell_in 1 on day 1', () => {
+    it('should have sell_in 1', () => {
       assert.strictEqual(items[1].sell_in, 1, 'Failed');
     });
-    it('should have quality 1 on day 1', () => {
+    it('should have quality 1', () => {
       assert.strictEqual(items[1].quality, 1, 'Failed');
     });
   });
@@ -52,10 +52,10 @@ describe('Aged Brie', () => {
   // (Does brie increase 2x as fast once the sell by has past..? due to everything else degrading 2x.. hmm.)
   // It does increase 2x as fast.
   describe('(0,0) - past sell by day', () => {
-    it('should have sell_in  on day 1', () => {
+    it('should have sell_in -1', () => {
       assert.strictEqual(items[2].sell_in, -1, 'Failed');
     });
-    it('should have quality 1 on day 1', () => {
+    it('should have quality 1', () => {
       assert.strictEqual(items[2].quality, 2, 'Failed');
     });
   });
@@ -64,15 +64,15 @@ describe('Aged Brie', () => {
 // Test Hand of Rag
 describe('Sulfuras, Hand of Ragnaros', () => {
   describe('(0,80) - Regular ', () => {
-    it('should have sell_in 0 on day 1', () => {
+    it('should have sell_in 0', () => {
       assert.strictEqual(items[3].sell_in, 0, 'Failed');
     });
-    it('should have quality 80 on day 1', () => {
+    it('should have quality 80', () => {
       assert.strictEqual(items[3].quality, 80, 'Failed');
     });
   });
   describe('(-1,80) - no double decay past sell by date', () => {
-    it('should have quality 80 on day 1', () => {
+    it('should have quality 80', () => {
       assert.strictEqual(items[4].quality, 80, 'Failed');
     });
   });
@@ -81,45 +81,44 @@ describe('Sulfuras, Hand of Ragnaros', () => {
 // Test TAFKAL80ETC
 describe('TAFKAL80ETC', () => {    
   describe('(15, 20) - Normal', () => {
-    it('should have sell_in 14 on day 1', () => {
+    it('should have sell_in 14', () => {
       assert.strictEqual(items[5].sell_in, 14, 'Failed');
     });
-    it('should have quality 21 on day 1', () => {
+    it('should have quality 21', () => {
       assert.strictEqual(items[5].quality, 21, 'Failed');
     });
   });
   // Has its own quality system
   describe('(15,50) - Dont pass 50 quality', () => {
-    it('should have quality 50 on day 1', () => {
+    it('should have quality 50', () => {
       assert.strictEqual(items[6].quality, 50, 'Failed');
     });
   });
   // Increase by 2
   describe('(11,0) - quality +2 when day <= 10', () => {
-    it('should have sell_in 10 on day 1', () => {
+    it('should have sell_in 10', () => {
       assert.strictEqual(items[7].sell_in, 10, 'Failed');
     });
-    it('should have quality 2 on day 1', () => {
+    it('should have quality 2', () => {
       assert.strictEqual(items[7].quality, 2, 'Failed');
     });
   });
   // increase by 3
   describe('(6,0) - quality +3 when day <= 5', () => {
-    it('should have sell_in 5 on day 1', () => {
+    it('should have sell_in 5', () => {
       assert.strictEqual(items[8].sell_in, 5, 'Failed');
     });
-    it('should have quality 3 on day 1', () => {
+    it('should have quality 3', () => {
       assert.strictEqual(items[8].quality, 3, 'Failed');
     });
   });
   // Date Passes
-  describe('(0,50) - past concert ', () => {
-    it('should have sell_in -1 on day 1', () => {
-      assert.strictEqual(items[9].sell_in, -1, 'Failed');
+  describe('(0,40) - past concert ', () => {
+    it('should have sell_in -1  ', () => {
+      assert.strictEqual(items[9].sell_in, -1, 'Failed'); 
     });
-    it('should have quality 0 on day 1', () => {
+    it('should have quality 0', () => {
       assert.strictEqual(items[9].quality, 0, 'Failed');
     });
   });
-  });
-
+}); 
