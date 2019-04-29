00f00b74750672cc2dd725867bb974a363d917d7
Spencer Davis <spencerdavis2012@gmail.com>
2019-04-29 18:37:19 -0500

    made the function to pass to the html page the same as the one being tested

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -4,66 +4,56 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
-var items = []
+let items = []
 
-var update_quality  = (items) => {
-  for (var i = 0; i < items.length; i++) {
-    // Sell_in 
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          // Quality above zero and not Brie or Tickets or Hand of Rag
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        // Brie / Tickets
-        items[i].quality = items[i].quality + 1;
+const update_quality = (items) => {
 
-        // TICKETS START
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-        }
-        // TICKETS END
-      } // if 50 or above do nothing
-    } // End first statement
-
-
-    // For items that are past sell by date
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1;
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality;
-        }
-      } else {
-        
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
-        }
-      }
-    }
+  const qualityModifer = (item, change) => {
+    // Increase or decrease quality
+    item.quality = item.quality + change;
+    // If over 50, set to 50, else keep it where it is at
+    item.quality = item.quality > 50 ? 50 : item.quality;
+    // If Quality is less than zero, set it to zero.
+    item.quality < 0 && (item.quality = 0);
   }
-}
 
+  items.map((currentItem) => {
+    // Decrement sell_in
+    currentItem.name != 'Sulfuras, Hand of Ragnaros' && currentItem.sell_in--;
+    switch (currentItem.name){
+      case 'Aged Brie':
+        qualityModifer(currentItem, 1);
+        break;
+      case 'Sulfuras, Hand of Ragnaros':
+        // Do Nothing as Hand of Rag doesn't Change
+        break;
+      case 'Backstage passes to a TAFKAL80ETC concert':
+        if (currentItem.sell_in > 10){
+          // Regular +1
+          qualityModifer(currentItem, 1);
+        } else if (currentItem.sell_in > 5){
+          // Close to date +2
+          qualityModifer(currentItem, 2);
+        } else if (currentItem.sell_in >= 0){
+          // Super close to date +3
+          qualityModifer(currentItem, 3);
+        } else {
+          // Concert date has past, 0 quality
+          currentItem.quality = 0;
+        }
+        break;
+      default:
+        // If a conjured item
+        if (currentItem.name.toLowerCase().includes('conjured')){
+          currentItem.sell_in >= 0 ? qualityModifer(currentItem, -2) : qualityModifer(currentItem, -4);
+        } else {
+          // Regular item
+          currentItem.sell_in >= 0 ? qualityModifer(currentItem, -1) : qualityModifer(currentItem, -2);
+        }
+      break;
+    }
+  });
+} 
 
 module.exports = {
   
@@ -96,19 +86,25 @@ module.exports = {
           break;
         case 'Backstage passes to a TAFKAL80ETC concert':
           if (currentItem.sell_in > 10){
+            // Regular +1
             qualityModifer(currentItem, 1);
           } else if (currentItem.sell_in > 5){
+            // Close to date +2
             qualityModifer(currentItem, 2);
           } else if (currentItem.sell_in >= 0){
+            // Super close to date +3
             qualityModifer(currentItem, 3);
           } else {
+            // Concert date has past, 0 quality
             currentItem.quality = 0;
           }
           break;
         default:
+          // If a conjured item
           if (currentItem.name.toLowerCase().includes('conjured')){
             currentItem.sell_in >= 0 ? qualityModifer(currentItem, -2) : qualityModifer(currentItem, -4);
           } else {
+            // Regular item
             currentItem.sell_in >= 0 ? qualityModifer(currentItem, -1) : qualityModifer(currentItem, -2);
           }
         break;
