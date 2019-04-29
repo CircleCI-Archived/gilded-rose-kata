b3c6e85405f8b41973e4e3ffb5f32a4d01780041
Spencer Davis <spencerdavis2012@gmail.com>
2019-04-28 19:05:23 -0500

    got first mocha test up and running and passing

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,7 +6,7 @@ function Item(name, sell_in, quality) {
 
 var items = []
 
-function update_quality() {
+var update_quality = () => {
   for (var i = 0; i < items.length; i++) {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
@@ -34,6 +34,7 @@ function update_quality() {
     if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
       items[i].sell_in = items[i].sell_in - 1;
     }
+    // 
     if (items[i].sell_in < 0) {
       if (items[i].name != 'Aged Brie') {
         if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -48,6 +49,62 @@ function update_quality() {
       } else {
         if (items[i].quality < 50) {
           items[i].quality = items[i].quality + 1
+        }
+      }
+    }
+  }
+}
+
+module.exports = {
+  Item: function Item(name, sell_in, quality) {
+    this.name = name;
+    this.sell_in = sell_in;
+    this.quality = quality;
+  },
+  UpdateItems: update_quality = (items) => {
+    for (var i = 0; i < items.length; i++) {
+      if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (items[i].quality > 0) {
+          if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            items[i].quality = items[i].quality - 1
+          }
+        }
+      } else {
+        if (items[i].quality < 50) {
+          items[i].quality = items[i].quality + 1
+          if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+            if (items[i].sell_in < 11) {
+              if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1
+              }
+            }
+            if (items[i].sell_in < 6) {
+              if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1
+              }
+            }
+          }
+        }
+      }
+      if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        items[i].sell_in = items[i].sell_in - 1;
+      }
+      // 
+      if (items[i].sell_in < 0) {
+        if (items[i].name != 'Aged Brie') {
+          if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (items[i].quality > 0) {
+              if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                items[i].quality = items[i].quality - 1
+              }
+            }
+          } else {
+            items[i].quality = items[i].quality - items[i].quality
+          }
+        } else {
+          if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1
+          }
         }
       }
     }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,25 @@
+var gilded = require('../src/gilded_rose');
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+
+let items = [];
+
+items.push(new gilded.Item('+5 Dexterity Vest', 10, 20));
+items.push(new gilded.Item('Aged Brie', 2, 0));
+items.push(new gilded.Item('Elixir of the Mongoose', 5, 7));
+items.push(new gilded.Item('Sulfuras, Hand of Ragnaros', 0, 80));
+items.push(new gilded.Item('Sulfuras, Hand of Ragnaros', -1, 80));
+items.push(new gilded.Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
+items.push(new gilded.Item('Backstage passes to a TAFKAL80ETC concert', 10, 49));
+items.push(new gilded.Item('Backstage passes to a TAFKAL80ETC concert', 5, 49));
+
+gilded.UpdateItems(items);
+describe('Day 1', () => {
+  describe('Dexterity Vest', () => {
+    it('should have sell_in 9 on day 1', () => {
+      assert.equal(items[0].sell_in, 9);
+    });
+    it('should have quality 19 on day 1', () => {
+      assert.equal(items[0].quality, 19);
     });
   });
 });
